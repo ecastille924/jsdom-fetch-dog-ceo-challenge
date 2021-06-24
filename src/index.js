df55293a3a7ec0ab73629bd8cfca24d77e8452a3
Erick Castille <ecastille924@gmail.com>
2021-06-24 19:09:36 -0400

    commit tree 36ba6000dc0d227f1467c3b1a991ea4c19b0574c

--- src/index.js
+++ src/index.js
@@ -67,30 +67,3 @@ function filterBreeds(event){
         }
     }  
 }
-
-
-
-
-
-// Challenge 2
-// - on page load, fetches all the dog breeds using the url above ⬆️
-// - adds the breeds to the page in the `<ul>` provided in `index.html`
-
-// ## Challenge 3
-
-// Once all of the breeds are rendered in the `<ul>`, add JavaScript so that, when
-// the user clicks on any one of the `<li>`s, the font color of that `<li>`
-// changes. This can be a color of your choosing.
-
-// ---
-
-// ## Challenge 4
-
-// Once we are able to load _all_ of the dog breeds onto the page, add JavaScript
-// so that the user can filter breeds that start with a particular letter using a
-// dropdown.
-
-// For example, if the user selects 'a' in the dropdown, only show the breeds with
-// names that start with the letter a. For simplicity, the dropdown only includes
-// the letters a-d. However, we can imagine expanding this to include the entire
-// alphabet.
